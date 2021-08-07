5dae26998462487a46b52814fe4938322e4853dd
AMCDanyMX <danielalejandromoralescastillo@gmail.com>
2021-08-07 01:04:46 -0500

    version1

---
+++ js/index.js
@@ -0,0 +1,30 @@
+window.sr = ScrollReveal();
+  sr.reveal('.info', {
+    duration: 4000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.info img', {
+    duration: 4000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+  
+  window.sr = ScrollReveal();
+  sr.reveal('.info p', {
+    duration: 7000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+
+  window.sr = ScrollReveal();
+  sr.reveal('.links', {
+    duration: 5000,
+    origin: 'end',
+    distance: '-100px'
+  });
+
